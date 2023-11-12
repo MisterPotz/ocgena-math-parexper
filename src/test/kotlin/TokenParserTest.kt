4d2af19d3aeba51fe152ed26921704b8795913a8
Alexander Gornostaev <aleksgornostaev@gmail.com>
2023-11-12 13:14:50 +0300

    add ability to get variable names of an expression fixed bug with nested brackets

--- src/test/kotlin/TokenParserTest.kt
+++ src/test/kotlin/TokenParserTest.kt
@@ -1,13 +1,13 @@
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
-import ru.misterpotz.expression.facade.m
+import ru.misterpotz.expression.facade.fullConvertM
 import ru.misterpotz.expression.paramspace.VariableParameterSpace
 
 class TokenParserTest {
 
     @Test
     fun stringExpressionIsParsedAndEvaluated() {
-        val expression = "-10*4+250*(-1)-(-2*20+4)".m
+        val expression = "-10*4+250*(-1)-(-2*20+4)".fullConvertM
         val expected = -254.0
 
         Assertions.assertEquals(expected, expression.evaluate())
@@ -15,7 +15,7 @@ class TokenParserTest {
 
     @Test
     fun expressionWithVariableIsEvaluated() {
-        val expression = "5+(-  kek ) *34  - 3".m
+        val expression = "5+(-  kek ) *34  - 3".fullConvertM
         Assertions.assertEquals(
             -100.0,
             expression.evaluate(
@@ -27,8 +27,27 @@ class TokenParserTest {
     }
 
     @Test
+    fun doubleNumbersWorkOk() {
+        val expression = "(5.5 +(-  kek ) *34  - 3) * 0.5".fullConvertM
+        Assertions.assertEquals(
+            -49.75,
+            expression.evaluate(
+                parameterSpace = VariableParameterSpace(
+                    "kek" to 3.0
+                )
+            )
+        )
+    }
+
+    @Test
+    fun nestedBracksWorkOk() {
+        val expression = "((((((-4) + 4) * 3 - 20)*2)-4))".fullConvertM
+        Assertions.assertEquals(-44.0, expression.evaluate())
+    }
+
+    @Test
     fun throwsIfNoValueForVariablePassed() {
-        val expression = "5+(-  kek ) *34  - 3".m
+        val expression = "5+(-  kek ) *34  - 3".fullConvertM
 
         Assertions.assertThrows(NullPointerException::class.java) {
             expression.evaluate(VariableParameterSpace())
